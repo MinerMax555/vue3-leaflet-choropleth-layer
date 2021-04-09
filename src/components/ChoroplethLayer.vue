a0c81184ac710ada173fa84325e31b9a184963b3
Max Walder <max.walder@protonmail.com>
2021-04-09 11:23:39 +0200

    changes

---
+++ src/components/ChoroplethLayer.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Say hi!</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ChoroplethLayer.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
--- testing/App.vue
+++ testing/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <l-map></l-map>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LMap
   }
 }
 </script>
